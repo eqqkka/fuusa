5d28cc2de96e4a9363cc4faa054f5319237452c4
eqqkka <nechepurenkoegor123@gmail.com>
2025-05-07 23:57:44 +0600

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import threading
 import time
 
 # Токен
-TOKEN = "7507582678:AAFyHxxz8ymeQB9RmiE17gBGMigWuT1c4VY"
+TOKEN = "7507582678:AAGo5Ry4abMM4luvFkNEfjje0saT0wOgytI"
 if not TOKEN:
     raise Exception("BOT_TOKEN")
 
